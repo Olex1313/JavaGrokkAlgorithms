8a7f75df5c60df4254f1da19cff8ded57ebcbe29
Olex1313 <aalimonov_2@miem.hse.ru>
2021-07-03 09:24:24 +0300

    Add Recursion module, add simple countdown realization

---
+++ Recursion/src/Countdown.java
@@ -0,0 +1,16 @@
+public class Countdown {
+    public static void countdown(int i) {
+        System.out.println(Integer.toString(i) + " Iterations left");
+        if (i <= 0)
+            return;
+        else
+            countdown(i - 1);
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Testing countdown:");
+        countdown(5);
+        countdown(0);
+        countdown(3);
+    }
+}
