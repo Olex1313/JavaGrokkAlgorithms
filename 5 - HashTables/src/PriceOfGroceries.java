a386defdf6556e2ca67fbdeef32065e8b79efa3f
Olex1313 <aalimonov_2@miem.hse.ru>
2021-07-03 15:53:07 +0300

    Add HashTables chapter

---
+++ 5 - HashTables/src/PriceOfGroceries.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PriceOfGroceries {
+
+    public static void main(String[] args) {
+        Map<String, Double> book = new HashMap<>();
+
+        book.put("apple", 0.67); // an apple costs 67 cents
+        book.put("milk", 1.49); // milk costs $1.49
+        book.put("avocado", 1.49);
+
+        System.out.println(book); // {apple=0.67, avocado=1.49, milk=1.49}
+    }
+}
