752d5c37df2227b3c7198514926904a92a0ed07b
Olex1313 <aalimonov_2@miem.hse.ru>
2021-07-04 17:30:57 +0300

    Add BFS realization

---
+++ 6 - BFS/src/BreadthFirstSearch.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public Integer value;
+    public Node left, right;
+
+    public Node(Integer value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    public Node root;
+
+    public BinaryTree(Integer value) {
+        this.root = new Node(value);
+    }
+
+    public BinaryTree() {
+        this.root = null;
+    }
+}
+
+public class BreadthFirstSearch {
+    public static Boolean search(BinaryTree tree, Integer value) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(tree.root);
+        while (!q.isEmpty()) {
+            Node tmp = q.remove();
+            if (tmp.value == value)
+                return true;
+            if (tmp.left != null)
+                q.add(tmp.left);
+            if (tmp.right != null)
+                q.add(tmp.right);
+        }
+        return false;
+    }
+
+
+    public static void main(String args[]) {
+        BinaryTree tree = new BinaryTree(1);
+        tree.root.left = new Node(2);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node (4);
+        tree.root.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        System.out.printf(Boolean.toString(BreadthFirstSearch.search(tree, 1)));
+    }
+}
